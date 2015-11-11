1ab90a4b973c452b859d062a49d2dd1fac095861
김현재 <kimhun456@gmail.com>
2015-11-11 10:24:05 +0900

    template

---
+++ Android/app/src/main/java/org/secmem/swm_online/View/QRActivity.java
@@ -0,0 +1,15 @@
+package org.secmem.swm_online.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.secmem.swm_online.R;
+
+public class QRActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+    }
+}
