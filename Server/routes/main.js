3ab1cb5b0c1718466b700f60b87ff51244ab4783
ByungKwon Do <thomas.bk.do@gmail.com>
2015-12-05 21:30:39 +0900

    2015.12.05 add by DBK -add login session

--- Server/routes/main.js
+++ Server/routes/main.js
@@ -3,10 +3,22 @@
  */
 var express = require('express');
 var router = express.Router();
-
+/*
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
+
     res.render('main', { title: '메인' });
+
 });
+
+function ensureAuthenticated(req, res, next) {
+    console.log("ensureAuthenticated");
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // 로그인이 안되어 있으면, login 페이지로 진행
+    res.redirect('/');
+}
 
 module.exports = router;
