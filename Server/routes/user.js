72c96ed246011f22f2ab03e73712a8d0b0c2f7c0
dongwonKim <luffy9715@gmail.com>
2015-12-14 22:29:08 +0900

    DB connect

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -8,7 +8,13 @@ var router = express.Router();
 var util = require('./util');
 
 router.get('/info', util.ensureAuthenticated, function(req, res, next) {
-    res.render('user_info', { title: '사용자 정보' });
+    var query = 'select u_id,u_name,u_sex,u_period,u_device,u_birth,u_photo_url from t_user where u_id="' + req.session.passport.user.id + '"';
+    con.query(query, function(err, response){
+        console.log(response);
+        var send = JSON.stringify(response);
+        console.log(JSON.parse(send));
+        res.render('user_info', {title: '사용자 정보',result:JSON.parse(send)});
+    });
 });
 
 module.exports = router;
