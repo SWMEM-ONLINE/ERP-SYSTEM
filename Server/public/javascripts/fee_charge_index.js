3724a39052da09461c3246b9dd382b3f6892ea4f
dongwonKim <luffy9715@gmail.com>
2015-12-15 16:09:28 +0900

    Modify page template with type button and content input.

--- Server/public/javascripts/fee_charge_index.js
+++ Server/public/javascripts/fee_charge_index.js
@@ -56,29 +56,38 @@ function addList() {
     var lastRow = table.rows.length;
     var newRow = table.insertRow(lastRow);
     var date = newRow.insertCell(0);
-    var category = newRow.insertCell(1);
-    var members = newRow.insertCell(2);
-    var price = newRow.insertCell(3);
-    var addBtn = newRow.insertCell(4);
+    var type = newRow.insertCell(1);
+    var content = newRow.insertCell(2);
+    var members = newRow.insertCell(3);
+    var price = newRow.insertCell(4);
+    var addBtn = newRow.insertCell(5);
 
     var str0 = '<input id="date_'+rowCount+'" type="text" placeholder="날짜선택" onclick="calendar(this)" class="datepicker" readonly="readonly">';
     date.innerHTML = str0;
-
-    var str1 ='<input id="category_'+rowCount+'" type="text" placeholder="카테고리">';
-    category.innerHTML = str1;
-    var str2 = '<input id="members_'+rowCount+'" type="text" placeholder="회원선택" readonly="readonly"  onclick="memberSelect(this)">';
-    members.innerHTML = str2;
-    var str3 = '<input id="price_' + rowCount + '" type="text" placeholder="금액만 입력", onkeydown="isNumberKey(this)"/>';
-    price.innerHTML = str3;
-    var str4 = '<button id="plus" type="button" onclick="addList()" class="plusminus">+</button>';
-    addBtn.innerHTML = str4;
+    var str1 = '<div class="btn-group"><button id="type_'+rowCount+'" type="button" data-toggle="dropdown" class="dropdown-toggle" aria-expanded="false">구분</button><ul class="dropdown-menu"><li id="type1_'+rowCount+'" onclick="typecheck(this.id)"><a>회비</a></li><li id="type2_'+rowCount+'" onclick="typecheck(this.id)"><a>삼과비</a></li><li id="type3_'+rowCount+'" onclick="typecheck(this.id)"><a>기타</a></li></ul></div>';
+    type.innerHTML = str1;
+    var str2 ='<input id="content'+rowCount+'" type="text" placeholder="내용">';
+    content.innerHTML = str2;
+    var str3 = '<input id="members_'+rowCount+'" type="text" placeholder="회원선택" readonly="readonly"  onclick="memberSelect(this)">';
+    members.innerHTML = str3;
+    var str4 = '<input id="price_' + rowCount + '" type="text" placeholder="금액만 입력", onkeydown="isNumberKey(this)"/>';
+    price.innerHTML = str4;
+    var str5 = '<button id="plus" type="button" onclick="addList()" class="plusminus">+</button>';
+    addBtn.innerHTML = str5;
 
     var Allrows = document.getElementById('addlist').rows;
-    Allrows[lastRow-1].deleteCell(4);
-    var changedRow = Allrows[lastRow-1].insertCell(4);
+    Allrows[lastRow-1].deleteCell(5);
+    var changedRow = Allrows[lastRow-1].insertCell(5);
     changedRow.innerHTML = '<button id="minus" type="button" onclick="deleteRow(this)" class="plusminus">－</button>';
 
     rowCount = rowCount + 1;
+}
+
+function typecheck(string){
+    var id = string.substr(6);
+    var type = 'button#type_'+id;
+    var drop = '#'+string;
+    $(type).html($(drop).children('a').html());
 }
 
 function deleteRow(obj){
