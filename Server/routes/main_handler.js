eab2cfe554f6406743a2ae0fc61fa9538d9bc582
jung-inchul <brakeout7@naver.com>
2016-01-05 03:49:15 +0900

    adjust main page

--- Server/routes/main_handler.js
+++ Server/routes/main_handler.js
@@ -2,7 +2,7 @@
  * Created by jung-inchul on 2015. 12. 15..
  */
 function loadBookMain(con, req, res){
-    var query = 'select b.b_name name, DATEDIFF(CURDATE(), b.b_due_date) diff from t_book_rental a inner join t_book b on a.br_book_id=b.b_id where br_user=' + req.session.passport.user.id + '"';
+    var query = 'select b.b_name name, DATEDIFF(CURDATE(), b.b_due_date) diff from t_book_rental a inner join t_book b on a.br_book_id=b.b_id where br_user="' + req.session.passport.user.id + '"';
     con.query(query, function(err, response){
         res.send(response);
     });
