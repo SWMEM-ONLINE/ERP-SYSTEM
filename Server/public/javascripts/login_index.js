8340c0e7337b8b3d5b05450c960dabeb2bba28a0
dongwonKim <luffy9715@gmail.com>
2016-01-05 21:29:19 +0900

    login device information.

---
+++ Server/public/javascripts/login_index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by KIMDONGWON on 2016-01-05.
+ */
+$('#loginButton').click(function(){
+    var broswerInfo = navigator.userAgent;
+    if(broswerInfo.indexOf("APP_SWMEM_ANDROID")>-1){
+        alert($('#login_id').val());
+        window.SWMEM_ANDROID.sendDeviceAndToken($('#login_id').val());
+    }
+});
