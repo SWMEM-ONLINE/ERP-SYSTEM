e4aa7b8b8d1def388c86ea0f2e006a0bfa7afeac
jung-inchul <brakeout7@naver.com>
2016-01-05 22:07:34 +0900

    edit apply new book part.

--- Server/public/javascripts/apply_newbook_index.js
+++ Server/public/javascripts/apply_newbook_index.js
@@ -48,19 +48,9 @@ $('#newbookSearchBtn').click(function() {
     }
 });
 
-$('#loadMyapplication').click(function(){
-    $.post('/apply/newbook/loadMyapply', function(res){
-        settingHTML(res, 1);
-    });
-});
-
-function settingHTML(datalist, flag){
+function settingHTML(datalist){
     if(datalist.length === 0){
-        if(flag === 0){
-            toastr['info']('검사 결과가 없습니다');
-        }else{
-            toastr['info']('신청하신 책이 없습니다');
-        }
+        toastr['info']('검사 결과가 없습니다');
     }else {
         $.post('/apply/newbook/checkDuplication', function(response){
             var htmlString = '<tbody>';
@@ -70,51 +60,38 @@ function settingHTML(datalist, flag){
                 } else {
                     htmlString += '<tr>';
                 }
-                if (flag === 0) {
-                    htmlString += '<td><img class="bookSmallImg" src="' + data.coverLargeUrl + '"></td>';
-                    htmlString += '<td><div class="bookInfo">';
-                    htmlString += '<h4 class="bookTitle">' + data.title +'</h4>';
-                    for(var i = 0; i < response.length; i++){
-                        if(response[i].b_isbn === data.isbn){
-                            htmlString += '<p><span class="label label-danger">' + response[i].cnt + '권 존재</span></p>';
-                        }else if(response[i].cnt === data.isbn){
-                            htmlString += '<p><span class="label label-primary">신청중</span></p>';
-                            break;
-                        }
+                htmlString += '<td><img class="bookSmallImg" src="' + data.coverLargeUrl + '"></td>';
+                htmlString += '<td><div class="bookInfo">';
+                htmlString += '<h4 class="bookTitle">' + data.title +'</h4>';
+                console.log(response);
+                for(var i = 0; i < response.length; i++){
+                    if(response[i].b_isbn === data.isbn){
+                        htmlString += '<p><span class="label label-danger">' + response[i].cnt + '권 존재</span></p>';
+                    }else if(response[i].cnt === data.isbn){
+                        htmlString += '<p><span class="label label-primary">신청중</span></p>';
+                        break;
                     }
-                    htmlString += '<p>' + ' 저자 : ' + data.author + '</p><p>' + " 출판사 : " + data.publisher + '</p><p>' + "정가 : " + data.priceStandard + '원</p></div></td>';
-                    htmlString += '</tr>';
-                } else {
-                    htmlString += '<td><img class="bookSmallImg" src="' + data.ba_photo_url + '"></td>';
-                    htmlString += '<td><div class="bookInfo">';
-                    htmlString += '<h4 class="bookTitle">' + data.ba_name + '</h4>';
-                    htmlString += '<p>' + ' 저자 : ' + data.ba_author + '</p><p>' + " 출판사 : " + data.ba_publisher + '</p><p>' + "정가 : " + data.ba_price + '원</p></div></td>';
-                    htmlString += '</tr>';
                 }
+                htmlString += '<p>' + ' 저자 : ' + data.author + '</p><p>' + " 출판사 : " + data.publisher + '</p><p>' + "정가 : " + data.priceStandard + '원</p></div></td>';
+                htmlString += '</tr>';
             });
 
             htmlString += '</tbody>';
             $('#newbooklist').html(htmlString);
-            if (flag === 0) {             // flag가 0인경우. 신청요청을 넣었을 때
-                var pagination_string = '';
-                var number = parseInt(datalist.totalResults / datalist.maxResults) + 1;
-                for (var i = 1; i <= number; i++) {
-                    pagination_string += '<li><a href="#">' + i + '</a></li>';
-                }
-                $('.pagination').html(pagination_string);
-                $('.modal-title').text('도서 신청하기');
-                $('#request').text('신청');
-            } else {                      // flag 가 1인경우. 신청 취소를 넣었을 경우이다.
-                $('.modal-title').text('도서신청 취소하기');
-                $('#request').text('신청 취소');
-                $('.pagination').html('');
+            var pagination_string = '';
+            var number = parseInt(datalist.totalResults / datalist.maxResults) + 1;
+            for (var i = 1; i <= number; i++) {
+                pagination_string += '<li><a href="#">' + i + '</a></li>';
             }
-            clickEvent(datalist, flag);
+            $('.pagination').html(pagination_string);
+            $('.modal-title').text('도서 신청하기');
+            $('#request').text('신청');
+            clickEvent(datalist);
         });
     }
 }
 
-function clickEvent(datalist, flag){
+function clickEvent(datalist){
     $('tr').click(function() {
         var index = $(this).index();
         var string = '';
@@ -131,6 +108,7 @@ function clickEvent(datalist, flag){
                 }
                 else{
                     toastr['info']('도서신청에 성공했습니다');
+                    settingHTML(datalist);
                 }
             });
             $('div.modal').modal('hide');
