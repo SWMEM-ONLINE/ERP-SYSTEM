808241b7b2da84518c0ad270052408c4a07b1f38
dongwonKim <luffy9715@gmail.com>
2016-01-25 16:44:21 +0900

    .

--- Server/routes/images.js
+++ Server/routes/images.js
@@ -12,11 +12,11 @@ var util = require('util');
 router.get('/', function(req, res, next) {
     console.log("name:"+req.query.name); // form fields
     var name = req.query.name;
-    fs.stat("./uploads/"+name, function(err, stat) {
+    fs.stat("./Server/uploads/"+name, function(err, stat) {
         if (err) {
             console.log(err);
             name = 'noImage.jpg';
-            fs.stat("./uploads/"+name, function(err, stat) {
+            fs.stat("./Server/uploads/"+name, function(err, stat) {
                 if (err) {
                     console.log(err);
                     res.json({status:'1'});
@@ -27,7 +27,7 @@ router.get('/', function(req, res, next) {
                         'Content-Type' : 'image/' + ext,
                         'Content-Length' : stat.size
                     });
-                    rs = fs.createReadStream("./uploads/" + name); // public/img.jpg을 읽는다
+                    rs = fs.createReadStream("./Server/uploads/" + name); // public/img.jpg을 읽는다
                     util.pump(rs, res, function(err) {
                         if(err) {
                             console.log(err);
@@ -43,7 +43,7 @@ router.get('/', function(req, res, next) {
                 'Content-Type' : 'image/' + ext,
                 'Content-Length' : stat.size
             });
-            rs = fs.createReadStream("./uploads/" + name); // public/img.jpg을 읽는다
+            rs = fs.createReadStream("./Server/uploads/" + name); // public/img.jpg을 읽는다
             util.pump(rs, res, function(err) {
                 if(err) {
                     console.log(err);
