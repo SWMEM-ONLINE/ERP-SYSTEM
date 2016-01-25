ce7cd8b7b33b3235a10132d11e76c2e2cb4cca40
dongwonKim <luffy9715@gmail.com>
2016-01-25 16:10:43 +0900

    test

--- Server/app.js
+++ Server/app.js
@@ -50,7 +50,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer({ dest: './uploads/',
+app.use(multer({ dest: 'uploads/',
   rename: function (fieldname, filename) {
 
     return 'image'+Date.now();
