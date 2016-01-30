edb937fee0545bd67d78b765ade6b91718676216
jung-inchul <brakeout7@naver.com>
2016-01-30 16:06:59 +0900

    change DB column in t_schedule

--- DB/create_table.sql
+++ DB/create_table.sql
@@ -341,8 +341,8 @@ DROP TABLE IF EXISTS `t_schedule`;
         s_id int NOT NULL auto_increment primary key,
         s_title VARCHAR(50) NOT NULL,
         s_enroll_user VARCHAR(20),
-        s_start_date datetime NOT NULL,
-        s_end_date datetime NOT NULL,
+        s_start_date VARCHAR(20) NOT NULL,
+        s_end_date VARCHAR(20) NOT NULL,
         s_flag int NOT NULL,
         FOREIGN KEY(s_enroll_user) REFERENCES t_user(u_id)
     );
